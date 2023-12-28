c78bdbc0198e90ef71598e0dce3518d244bf9cf3
ChaeyeonHan <kate2236e@naver.com>
2023-12-28 19:47:24 +0900

    inital commit

---
+++ app/src/main/java/com/example/madcamp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.madcamp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun main(args: Array<String>) {
+        println("hello world!")
+
+    }
+}
