40b2816c51b054ac2efe4a8c20ea672912c6db08
ChaeyeonHan <kate2236e@naver.com>
2023-12-30 13:46:56 +0900

    json파일 읽어서 보여주기

--- app/src/main/java/com/example/madcamp/ProfileAdapter.kt
+++ app/src/main/java/com/example/madcamp/ProfileAdapter.kt
@@ -8,8 +8,12 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import org.w3c.dom.Text
 
-class ProfileAdapter(val profileList: ArrayList<Profile>):
+//class ProfileAdapter(val profileList: ArrayList<Profile>):
+
+class ProfileAdapter(val profileList: MutableList<Profile>):
         RecyclerView.Adapter<ProfileAdapter.ProfileViewHolder>() {
 
     // 뷰가 만들어질때 호출되는 메서드. 뷰홀더를 생성해 리턴
@@ -23,23 +27,46 @@ class ProfileAdapter(val profileList: ArrayList<Profile>):
     // 뷰와 내용 연결
     // recyclerview -> 재사용. 스크롤 내리거나 올릴때 호출
     override fun onBindViewHolder(holder: ProfileViewHolder, position: Int) {
-        holder.rv_image.text = profileList[position].image
-        holder.rv_name.text = profileList[position].name
-        holder.rv_phone.text = profileList[position].phone
+
+        val profile = profileList[position]
+
+        // Glide로 이미지 로드 및 표시
+
+        Glide.with(holder.itemView.context).clear(holder.rv_image)
+        Glide.with(holder.itemView.context)
+            .load(profile.image)
+//            .load("https://media.vlpt.us/images/sasy0113/post/f7085683-1a62-4ce7-9f7f-e8fd2f3ec825/Android%20Kotlin.jpg")
+            .override(100, 100)
+            .placeholder(R.drawable.ic_home)
+            .error(R.drawable.baseline_question_mark_24)
+            .into(holder.rv_image)
+
+//        holder.rv_image.text = profile.image
+        holder.rv_name.text = profile.name
+        holder.rv_phone.text = profile.phone
+//        holder.rv_name.text = profileList[position].name
+//        holder.rv_phone.text = profileList[position].phone
 
     }
     override fun getItemCount(): Int {
-        Log.d("size", profileList.size.toString())
+       // Log.d("size", profileList.size.toString())
         return profileList.count()
     }
 
 
     // 해당 뷰와 연결
     inner class ProfileViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val rv_image = itemView.findViewById<TextView>(R.id.tv_rv_photo)
+        var rv_image : ImageView = itemView.findViewById(R.id.tv_rv_photo)
+//        val rv_image = itemView.findViewById<TextView>(R.id.tv_rv_photo)
         val rv_name = itemView.findViewById<TextView>(R.id.tv_rv_name)
         val rv_phone = itemView.findViewById<TextView>(R.id.tv_rv_phone)
     }
 
+//    fun addItemDecoration(context: Context, recyclerView: RecyclerView) {
+//        val itemDecoration = DividerItemDecoration(context)
+//        recyclerView.addItemDecoration(itemDecoration)
+//
+////        recyclerView.addItemDecoration(DividerItemDecoration(context))
+//    }
 
 }
--- app/src/main/java/com/example/madcamp/Tab1.kt
+++ app/src/main/java/com/example/madcamp/Tab1.kt
@@ -1,15 +1,18 @@
 package com.example.madcamp
 
-import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.madcamp.databinding.FragmentTab1Binding
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.lang.reflect.Type
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,6 +28,8 @@ class Tab1 : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,18 +52,34 @@ class Tab1 : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val rv_profile = view.findViewById<RecyclerView>(R.id.rv_profile)
-        val profileList = ArrayList<Profile>()
-        profileList.add(Profile("image1", "이름1", "010-1234-5678" ))
-        profileList.add(Profile("image2", "이름2", "010-1234-5678" ))
-        profileList.add(Profile("image3", "이름3", "010-1234-5678" ))
-        Log.d("test", "profileList.add")
+        val profileAllData : MutableList<Profile> = mutableListOf()
+
+        val context = context ?:return
+        val jsonUtility = JsonUtility(context)
+        try {
+            val jsonData = jsonUtility.readJson("test.json")
+            val profileType: Type = object: TypeToken<List<Profile>>() {}.type
+            val profiles = jsonUtility.parseJson<List<Profile>>(jsonData, profileType)
+
+            profiles.forEach{
+                profileAllData.add(it)
+            }
+//            Log.d("ProfileList: ", "image: ${profileAllData[0][0].image}, name: ${profileAllData[0][0].name}, phone: ${profileAllData[0][0].phone}")
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+//        profileAllData.forEachIndexed()
 
         // ArrayList로 어댑터 만들고, 어댑터와 리사이클러뷰 갱신
-        val profileAdapter = ProfileAdapter(profileList)
+        val profileAdapter = ProfileAdapter(profileAllData)
 
         rv_profile.adapter = profileAdapter
         context?.let {
             rv_profile.layoutManager = LinearLayoutManager(it)
+            rv_profile.addItemDecoration(VerticalItemDecorator(10))
+//            profileAdapter.addItemDecoration(it, rv_profile)
         }
         profileAdapter.notifyDataSetChanged()
 
@@ -83,4 +104,18 @@ class Tab1 : Fragment() {
                 }
             }
     }
+}
+
+class JsonUtility(private val context: Context) {
+
+    fun readJson(fileName: String): String {
+        return context.assets.open(fileName).bufferedReader().use{it.readText()}
+    }
+
+    // gson - json 파싱과정을 간단하게
+    fun <T> parseJson(jsonData: String, clazz: Type): T {
+        val gson = Gson()
+        return gson.fromJson(jsonData, clazz)
+    }
+
 }
